06853912adddb6ad9f7d1f337c193d5a1ce3aee1
Your Name <your.email@example.com>
2025-04-13 19:28:01 +0400

    cron.js added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from "express"
 import cors from "cors"
 import "dotenv/config"
+import job from "./lib/cron.js"
 
 import authRoutes from "./routes/authRoutes.js"
 import bookRoutes from "./routes/bookRoutes.js"
@@ -12,6 +13,8 @@ const PORT = process.env.PORT || 3000
 
 app.use(express.json());
 app.use(cors());
+
+job.start();
 
 app.use("/api/auth", authRoutes);
 app.use("/api/books", bookRoutes);
---
+++ src/lib/cron.js
@@ -0,0 +1,18 @@
+import cron from "node-cron";
+import https from "https";
+
+const job = new cron.CronJob("*14 * * * *", function () {
+    https
+    .get(process.env.API_URL, (res) => {
+        if (res.statusCode === 200) {
+            console.log("Cron job executed successfully");
+        } else {
+            console.log("Cron job failed", res.statusCode);
+        }
+    })
+    .on("error", (err) => {
+        console.log("Cron job error sending request", err);
+    })
+})
+
+export default job;
